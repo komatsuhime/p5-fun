6faae62f568c34634881033e38caaf812a4a0e7f
Carla Faroldi <carla.faroldi@gmail.com>
2017-09-11 21:40:47 -0300

    Added random color generator.

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
+var r;
+var g;
+var b;
+
 function setup() {
-	createCanvas(640, 480);
+    createCanvas(640, 480);
+    r = random(256);
+    g = random(256);
+    b = random(256);
 }
 
 function draw() {
-	if (mouseIsPressed) {
-		fill(232,238,94);
-		triangle(mouseX, mouseY, mouseX+10, mouseY+20, mouseX+20, mouseY);
-	}
+    if (mouseIsPressed) {
+        fill(r, g, b);
+        noStroke();
+        triangle(mouseX, mouseY, mouseX + 10, mouseY + 20, mouseX + 20, mouseY);
+    }
 }
