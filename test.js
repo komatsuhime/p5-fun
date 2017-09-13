e15e610ab6a4c76b9517ebb2539d48ce8987781f
komatsuhime <carla.faroldi@gmail.com>
2017-09-13 19:27:58 -0300

    The number of stars generated is now limited.

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var r;
 var g;
 var b;
+var i = 0;
 
 function setup() {
     createCanvas(640, 480);
@@ -8,19 +9,24 @@ function setup() {
     g = random(256);
     b = random(256);
 
-    background(0,0,0);
+    background(0, 0, 0);
+
 }
 
 function draw() {
 
     if (mouseIsPressed) {
-    	fill(r, g, b);
+        fill(r, g, b);
         noStroke();
         triangle(mouseX, mouseY, mouseX + 10, mouseY + 20, mouseX + 20, mouseY);
     }
 
-    stroke(255, 255, 255);
-    fill(255, 255, 255);
-    point(random(641), random(481));
-    ellipse(random(641), random(481),random(5));
+    i++;
+    if (i < 100) {
+        stroke(255, 255, 255);
+        fill(255, 255, 255);
+        point(random(641), random(481));
+        ellipse(random(641), random(481), random(5));
+    }
+
 }
