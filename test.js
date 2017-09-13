7895cfde37e3b3732528e4c506de7050e516b89b
komatsuhime <carla.faroldi@gmail.com>
2017-09-12 22:58:23 -0300

    Added random dots all over the canvas.

--- test.js
+++ test.js
@@ -7,11 +7,15 @@ function setup() {
     r = random(256);
     g = random(256);
     b = random(256);
+
+    background(0,0,0);
 }
 
 function draw() {
+    stroke(255, 255, 255);
+    point(random(641), random(481));
     if (mouseIsPressed) {
-        fill(r, g, b);
+    	fill(r, g, b);
         noStroke();
         triangle(mouseX, mouseY, mouseX + 10, mouseY + 20, mouseX + 20, mouseY);
     }
